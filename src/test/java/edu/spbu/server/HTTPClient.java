60c8b030beb70df510d8bc65560d81dc2d5f7bd7
TiJayTJ <105743753+TiJayTJ@users.noreply.github.com>
2023-04-10 12:04:22 +0300

    add 400, 404 errors

--- src/test/java/edu/spbu/server/HTTPClient.java
+++ src/test/java/edu/spbu/server/HTTPClient.java
@@ -5,37 +5,58 @@ import java.net.Socket;
 
 public class HTTPClient {
 
-    public void request(String url, int port) throws IOException {
+    public void request(String url, int port) {
 
-        String[] fullUrl = url.strip().split("/", 2);
-        String getInfo = fullUrl[1];
-
-        Socket clientSocket = new Socket(url, port);
-
-        OutputStream output = clientSocket.getOutputStream();
-        InputStream input = clientSocket.getInputStream();
-
-        PrintStream print = new PrintStream(output);
-
-        print.printf("GET /%s HTTP/1.1%n", getInfo);
-        print.printf("Host: %s:%s%n%n", url, port);
-        print.println("");
-        print.flush();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-        String outStr;
-        while ((outStr = reader.readLine()) != null) {
-            System.out.println(outStr);
+        var fullUrl = url.strip().split("/", 2);
+        String getInfo;
+        if (fullUrl.length == 2) {
+            getInfo = fullUrl[1];
+        }
+        else if (fullUrl.length == 1) {
+            getInfo = "";
+        } else{
+            System.out.println("Incorrect input");
+            return;
+        }
+        
+        try(Socket clientSocket = new Socket(fullUrl[0], port)){
+            OutputStream output = clientSocket.getOutputStream();
+            InputStream input = clientSocket.getInputStream();
+    
+            PrintStream print = new PrintStream(output);
+    
+            print.printf("GET /%s HTTP/1.1%n", getInfo);
+            print.printf("Host: %s:%s%n%n", url, port);
+            print.println("");
+            print.flush();
+    
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+    
+            String outStr;
+            while ((outStr = reader.readLine()) != null) {
+                System.out.println(outStr);
+            }
+    
+            print.close();
+            reader.close();
+        } catch (IOException e){
+            e.printStackTrace();
         }
 
-        print.close();
-        reader.close();
+        
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
+        int hostPort = Integer.parseInt(args[0]);
+        int ePort = 80;
+        
         HTTPClient client = new HTTPClient();
-        client.request("localhost/exercise1.html", 8080);
+    
+        client.request("localhost/index.html", hostPort);
+        client.request("localhost/index.html", hostPort);
+        client.request("localhost", hostPort);
+        client.request("openai.com/blog/chatgpt", ePort);
+        client.request("openai.com/hfsd", ePort);
     }
 
 }
--- src/test/java/edu/spbu/server/HTTPServer.java
+++ src/test/java/edu/spbu/server/HTTPServer.java
@@ -1,38 +1,77 @@
 package edu.spbu.server;
 
-//import java.io.*;
-import javax.swing.table.JTableHeader;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class HTTPServer {
     
-    private int port;
-    private String directory;
-    public HTTPServer(int port, String directory){
-        this.port = port;
-        this.directory = directory;
-    }
-    void start(){
-        try (var server = new ServerSocket(this.port)){
-            while (true){
-                var socket = server.accept();
-                var thread = new Handler(socket, this.directory);
-                thread.start();
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
+    private static void sendResponse(OutputStream output, int statusCode, String statusText, long length){
+        PrintStream printer = new PrintStream(output);
+        printer.printf("HTTP/1.1 %s %s", statusCode, statusText);
+        printer.printf("Content-Type: %s%n", "text/html");
+        printer.printf("Content-Length: %s%n%n", length);
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        int count = 0;
         var port = Integer.parseInt(args[0]);
         var directory = args[1];
-        new HTTPServer(port, directory).start();
+        
+        try(ServerSocket serverSocket = new ServerSocket(port)){
+            while(true) {
+                var socket = serverSocket.accept();
+                System.out.println("Client accepted " + (count++));
+        
+                // streams creating
+                OutputStream output = socket.getOutputStream();
+                InputStream input = socket.getInputStream();
+        
+                // getting request
+                Scanner scanner = new Scanner(input).useDelimiter("\r\n");
+                String str = scanner.next();
+                System.out.println(str);
+                List<String> request = List.of(str.split(" "));
+    
+                // opening error files
+                Path error400 = Path.of(directory, "error400.html");
+                var error400Bytes = Files.readAllBytes(error400);
+                Path error404 = Path.of(directory, "error404.html");
+                var error404Bytes = Files.readAllBytes(error404);
+                
+                // checking for a get request
+                if(!Objects.equals(request.get(0), "GET")){
+                    sendResponse(output, 400, "Bad Request", error400Bytes.length);
+                    output.write(error400Bytes);
+                    return;
+                }
+                
+                // checking for the existence of a file
+                // and
+                // sending response
+                Path filePath = Path.of(directory, request.get(1));
+                if (Files.exists(filePath) && !Files.isDirectory(filePath)){
+                    var fileByte = Files.readAllBytes(filePath);
+                    sendResponse(output, 200, "OK", fileByte.length);
+                    output.write(fileByte);
+                }
+                else {
+                    sendResponse(output, 404, "Not found", error404Bytes.length);
+                    output.write(error404Bytes);
+                }
+                System.out.println(filePath);
+        
+                System.out.println("socket closed\n");
+                socket.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
